bc3dc2af563df062abaadfd14ffb2a869f873004
win <wintercoredev@gmail.com>
2015-03-08 01:20:29 +0700

    38.1 : JsTypeDefinitionBuilderBase

---
+++ VRoomJs2/3_MiniBridge/JsTypeDefinitionBuilder.cs
@@ -0,0 +1,23 @@
+﻿//2015, MIT ,WinterDev
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.IO;
+
+using VroomJs;
+
+namespace NativeV8
+{
+    
+
+    public abstract class JsTypeDefinitionBuilderBase
+    {
+        internal JsTypeDefinition BuildTypeDefinition(Type t)
+        {
+            return this.OnBuildRequest(t);
+        }
+        protected abstract JsTypeDefinition OnBuildRequest(Type t); 
+    }
+
+}
