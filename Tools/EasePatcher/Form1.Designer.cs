a52c7f34109d72ec24e3c27a0d74217134e33e10
enginekit <enginecollection@gmail.com>
2017-05-26 10:51:03 +0700

    152: init EasePatch

---
+++ Tools/EasePatcher/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace EasePatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdPatchEspresso = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdPatchEspresso
+            // 
+            this.cmdPatchEspresso.Location = new System.Drawing.Point(12, 12);
+            this.cmdPatchEspresso.Name = "cmdPatchEspresso";
+            this.cmdPatchEspresso.Size = new System.Drawing.Size(153, 38);
+            this.cmdPatchEspresso.TabIndex = 0;
+            this.cmdPatchEspresso.Text = "PatchEspresso";
+            this.cmdPatchEspresso.UseVisualStyleBackColor = true;
+            this.cmdPatchEspresso.Click += new System.EventHandler(this.cmdPatchEspresso_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 272);
+            this.Controls.Add(this.cmdPatchEspresso);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdPatchEspresso;
+    }
+}
+
