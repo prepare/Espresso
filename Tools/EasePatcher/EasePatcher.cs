dcb80607b395c84ec8cfbcf95573b7e841602b42
enginekit <enginecollection@gmail.com>
2017-05-27 05:49:38 +0700

    158: update patchers

--- Tools/EasePatcher/EasePatcher.cs
+++ Tools/EasePatcher/EasePatcher.cs
@@ -11,132 +11,45 @@ namespace EasePatcher
         Zero,
         InitBuild,
     }
-    class Patcher
+
+    enum PatcherOS
     {
-        string _original_node_src_dir;
-        string _espresso_src;
-        string _initBuildParameters;
-        bool _isWindowsBuild;
-        BuildState _buildState;
+        Unknown,
+        Windows,
+        Mac,
+        Linux
+
+    }
+
+    abstract class PatcherBase
+    {
+        protected string _original_node_src_dir;
+        protected string _espresso_src;
+
         public event EventHandler FinishInitBuild;
 
-        public void Setup(string original_node_src, string espresso_src, string initBuildParameters = "")
+        protected BuildState _buildState;
+        protected void InvokeFinishInitBuild()
         {
-            _buildState = BuildState.Zero;
-            _initBuildParameters = initBuildParameters;
-            this._original_node_src_dir = original_node_src;
-            this._espresso_src = espresso_src;
-            //check platform
-            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(
-                System.Runtime.InteropServices.OSPlatform.OSX) ||
-                System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(
-                System.Runtime.InteropServices.OSPlatform.Linux)
-                )
+            if (FinishInitBuild != null)
             {
-                //unix -style build
-                _isWindowsBuild = false;
-            }
-            else if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(
-                System.Runtime.InteropServices.OSPlatform.Windows))
-            {
-                //Windows
-                _isWindowsBuild = true;
-            }
-            else
-            {
-                _isWindowsBuild = false;
+                FinishInitBuild(this, EventArgs.Empty);
             }
         }
-        public void InitBuild(EventHandler finishInitBuild)
-        {
-            if (_isWindowsBuild)
-            {
-                //Windows
-                this.FinishInitBuild = finishInitBuild;
-                InitWindowsBuild();
-            }
-            else
-            {
-                this.FinishInitBuild = finishInitBuild;
-                InitUnixBuild();
-            }
-        }
+        /// <summary>
+        /// path to patch folder relative to main espresso src folder (eg. node_patches\node7.10_modified)
+        /// </summary>
+        public string PatchSubFolder { get; set; }
 
-        void InitWindowsBuild()
-        {
-            //-------------------------------------------------------
-            //FOR WINDOWS:....
-            //-------------------------------------------------------
-            //1. specific location of original node src
-            //2. run first FRESH build of the node src 
-            string vc_build_script = _original_node_src_dir + @"\vcbuild.bat";
-            if (!File.Exists(vc_build_script))
-            {
-                return;
-            }
 
-            _buildState = BuildState.InitBuild;
-
-            ThreadPool.QueueUserWorkItem(delegate
-            {
-                //choose x86 or x64;
-                //choose release or debug
-                ProcessStartInfo procStartInfo = new ProcessStartInfo(
-                    vc_build_script,
-                    _initBuildParameters);
-                //procStartInfo.UseShellExecute = false;
-                //procStartInfo.RedirectStandardOutput = true;
-                //procStartInfo.CreateNoWindow = true;
-                procStartInfo.WorkingDirectory = _original_node_src_dir;
-
-                Process proc = System.Diagnostics.Process.Start(procStartInfo);
-                proc.WaitForExit();
-                //finish
-                if (FinishInitBuild != null)
-                {
-                    FinishInitBuild(this, EventArgs.Empty);
-                }
-            });
-        }
-        void InitUnixBuild()
-        {
-            //1. configure
-            //2. make
-
-            ThreadPool.QueueUserWorkItem(delegate
-            {
-                if (FinishInitBuild != null)
-                {
-                    FinishInitBuild(this, EventArgs.Empty);
-                }
-                UnixConfigure();
-                UnixMake();
-            });
-
-        }
-        void UnixConfigure()
-        {
-            ProcessStartInfo stInfo = new ProcessStartInfo(_espresso_src + "/configure");
-            stInfo.WorkingDirectory = _original_node_src_dir;
-            //
-            Process proc = Process.Start(stInfo);
-            proc.WaitForExit();
-        }
-        void UnixMake()
-        {
-            ProcessStartInfo stInfo = new ProcessStartInfo("make");
-            stInfo.WorkingDirectory = _original_node_src_dir;
-            //
-            Process proc = Process.Start(stInfo);
-            proc.WaitForExit();
-        }
         public void DoPatch()
         {
             //1. copy file from espresso's patch folder 
             //and place into target dir 
             //the version must be match between original source and the patch
 
-            string patch_folder = _espresso_src + @"\node_patches\node7.10_modified";
+            //string patch_folder = _espresso_src + "\"+ @"node_patches\node7.10_modified";
+            string patch_folder = _espresso_src + "/" + PatchSubFolder;
 
             ReplaceFileInDirectory(patch_folder, _original_node_src_dir);
             //2. copy core of libespresso bridge code( nodejs and .NET)
@@ -145,8 +58,7 @@ namespace EasePatcher
             string targetDir = _original_node_src_dir + "/src/libespresso";
             if (Directory.Exists(targetDir))
             {
-                //should be 1 level
-                Directory.Delete(targetDir);
+                Directory.Delete(targetDir, true);
             }
             //create targetdir
             Directory.CreateDirectory(targetDir);
@@ -167,6 +79,7 @@ namespace EasePatcher
                 }
             }
         }
+
         static void ReplaceFileInDirectory(string patchSrcDir, string targetDir)
         {
             //recursive
@@ -202,7 +115,122 @@ namespace EasePatcher
             //----------------------------
             //replace src to dest
             File.Copy(patchFileName, targetToBeReplaceFileName, true);
+        }
 
+        public static PatcherOS GetPatcherOS()
+        {
+            //check platform
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(
+                System.Runtime.InteropServices.OSPlatform.OSX))
+            {
+                return PatcherOS.Mac;
+            }
+            else if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(
+                System.Runtime.InteropServices.OSPlatform.Linux)
+                )
+            {
+                return PatcherOS.Linux;
+            }
+            else if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(
+              System.Runtime.InteropServices.OSPlatform.Windows))
+            {
+                return PatcherOS.Windows;
+            }
+            else
+            {
+                return PatcherOS.Unknown;
+            }
         }
     }
+
+    class WindowsPatcher : PatcherBase
+    {
+        string _initBuildParameters;
+        public void Setup(string original_node_src,
+            string espresso_src,
+            string initBuildParameters = "")
+        {
+            _buildState = BuildState.Zero;
+            _initBuildParameters = initBuildParameters;
+            this._original_node_src_dir = original_node_src;
+            this._espresso_src = espresso_src;
+        }
+
+        public void Build()
+        {
+            //-------------------------------------------------------
+            //FOR WINDOWS:....
+            //-------------------------------------------------------
+            //1. specific location of original node src
+            //2. run first FRESH build of the node src 
+            string vc_build_script = _original_node_src_dir + @"\vcbuild.bat";
+            if (!File.Exists(vc_build_script))
+            {
+                return;
+            }
+
+            _buildState = BuildState.InitBuild;
+
+            ThreadPool.QueueUserWorkItem(delegate
+            {
+                //choose x86 or x64;
+                //choose release or debug
+                ProcessStartInfo procStartInfo = new ProcessStartInfo(
+                    vc_build_script,
+                    _initBuildParameters);
+                //procStartInfo.UseShellExecute = false;
+                //procStartInfo.RedirectStandardOutput = true;
+                //procStartInfo.CreateNoWindow = true;
+                procStartInfo.WorkingDirectory = _original_node_src_dir;
+
+                Process proc = System.Diagnostics.Process.Start(procStartInfo);
+                proc.WaitForExit();
+                //finish
+                InvokeFinishInitBuild();
+
+            });
+        }
+    }
+    class LinuxAndMacPatcher : PatcherBase
+    {
+        public void Setup(string original_node_src, string espresso_src, string initBuildParameters = "")
+        {
+            _buildState = BuildState.Zero;
+            this._original_node_src_dir = original_node_src;
+            this._espresso_src = espresso_src;
+
+            Build();
+        }
+
+        public void Build()
+        {
+            //1. configure
+            //2. make
+            _buildState = BuildState.InitBuild;
+            ThreadPool.QueueUserWorkItem(delegate
+            {
+                UnixConfigure();
+                UnixMake();
+                InvokeFinishInitBuild();
+            });
+
+        }
+        void UnixConfigure()
+        {
+            ProcessStartInfo stInfo = new ProcessStartInfo(_espresso_src + "/configure");
+            stInfo.WorkingDirectory = _original_node_src_dir;
+            //
+            Process proc = Process.Start(stInfo);
+            proc.WaitForExit();
+        }
+        void UnixMake()
+        {
+            ProcessStartInfo stInfo = new ProcessStartInfo("make");
+            stInfo.WorkingDirectory = _original_node_src_dir;
+            //
+            Process proc = Process.Start(stInfo);
+            proc.WaitForExit();
+        }
+    }
+
 }
