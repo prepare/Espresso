52bc9903772be96f725529092f484029d344adc6
enginekit <enginecollection@gmail.com>
2017-06-07 03:23:16 +0700

    222: patcher with simple menu

--- Tools/EasePatcher/Program.cs
+++ Tools/EasePatcher/Program.cs
@@ -1,18 +1,121 @@
 ﻿//MIT, 2017, EngineKit
 using System;
+using System.IO;
 
 namespace EasePatcher
 {
     class Program
     {
 
+        static string current_node_version = "node-v7.10.0";
+        static string patch_subdir = "node_patches/node7.10_modified";
+        //
+        static string original_node_srcdir = "";
+        static string espresso_srcdir = "";
+        static string config_pars = "";
+        //
+        static PatcherOS currentOS;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Espresso's EasePatcher");
-            Console.WriteLine("Start Build....");
-             
+            Console.WriteLine("for " + current_node_version);
+            Console.WriteLine("======================");
 
-            switch (PatcherBase.GetPatcherOS())
+            //------------------------------------------  
+            //welcome page....
+            InitCheckSourceAndTargetFolders();
+            //------------------------------------------         
+            //select what to do 
+
+            while (DisplayMainMenu()) ;
+
+        }
+        static void InitCheckSourceAndTargetFolders()
+        {
+
+            //1. os
+            currentOS = PatcherBase.GetPatcherOS();
+            Console.WriteLine("current OS: " + currentOS);
+
+
+            switch (currentOS)
+            {
+                case PatcherOS.Windows:
+
+                    original_node_srcdir = @"C:\projects\node-v7.10.0";
+                    espresso_srcdir = @"D:\projects\CompilerKit\Espresso";
+                    config_pars = "release dll nosign nobuild";
+
+                    break;
+                case PatcherOS.Mac:
+                    original_node_srcdir = "../../../node-v7.10.0";
+                    espresso_srcdir = "../../../Espresso";
+                    config_pars = "--dest-cpu=x64 --shared --xcode";
+
+                    break;
+                case PatcherOS.Linux:
+                    original_node_srcdir = "../../../node-v7.10.0";
+                    espresso_srcdir = "../../../Espresso";
+                    config_pars = "x64 --shared";
+                    break;
+            }
+            //
+            Console.Write("original_node_srcdir: " + original_node_srcdir + " ->");
+            if (!Directory.Exists(original_node_srcdir))
+            {
+                Console.WriteLine(" NOT FOUND! ");
+            }
+            else
+            {
+                Console.WriteLine("FOUND");
+            }
+            //
+            Console.Write("espresso_srcdir: " + espresso_srcdir + " ->");
+            if (!Directory.Exists(espresso_srcdir))
+            {
+                Console.WriteLine(" NOT FOUND! ");
+            }
+            else
+            {
+                Console.WriteLine("FOUND");
+            }
+
+            Console.WriteLine("======================");
+        }
+
+        static bool DisplayMainMenu()
+        {
+            Console.WriteLine("Choose What to do... (and press ENTER)");
+            Console.WriteLine();
+            Console.WriteLine(" 0 : Exit");
+            Console.WriteLine(" 1 : Apply Patches");
+            Console.WriteLine();
+            //wait
+            string userReadLine = Console.ReadLine();
+            int selected_menuNum;
+
+            if (!int.TryParse(userReadLine, out selected_menuNum))
+            {
+                //with tail call
+                return true;
+            }
+            //
+            switch (selected_menuNum)
+            {
+                case 0:
+                    Console.WriteLine("bye!");
+                    return false; //exit
+                case 1:
+                    ApplyPatches();
+                    break;
+            }
+            return true;
+        }
+        static void ApplyPatches(SimpleAction next = null)
+        {
+            Console.WriteLine("ApplyPatches ....");
+            switch (currentOS)
             {
                 case PatcherOS.Windows:
                     {
@@ -20,9 +123,9 @@ namespace EasePatcher
                         patcher.PatchSubFolder = "node_patches/node7.10_modified";
                         patcher.Setup(@"C:\projects\node-v7.10.0", //specific target 
                           @"D:\projects\CompilerKit\Espresso",
-                          "release dll nosign nobuild"); //we will build it manually with visual studio
-                        //
-                        //"nosign nobuild"); //we will build it manually with visual studio
+                          "release dll nosign nobuild");
+
+                        //we will build it manually with visual studio
 
                         Console.WriteLine("Building ...");
                         patcher.Configure(() =>
@@ -31,6 +134,10 @@ namespace EasePatcher
                             {
                                 patcher.DoPatch();
                                 Console.WriteLine("Finish!");
+                                if (next != null)
+                                {
+                                    next();
+                                }
                             }
                         });
                     }
@@ -49,6 +156,10 @@ namespace EasePatcher
                         {
                             patcher.DoPatch();
                             Console.WriteLine("Finish!");
+                            if (next != null)
+                            {
+                                next();
+                            }
                         });
 
                     }
@@ -66,14 +177,16 @@ namespace EasePatcher
                         {
                             patcher.DoPatch();
                             Console.WriteLine("Finish!");
+                            if (next != null)
+                            {
+                                next();
+                            }
                         });
                     }
                     break;
                 default:
                     throw new NotSupportedException();
             }
-
-            string userReadLine = Console.ReadLine();
         }
     }
 }
