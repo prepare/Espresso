4d43afee1985a9fd70b2dc9a83aa53f01a00fe51
win <wintercoredev@gmail.com>
2017-09-06 19:04:13 +0700

    258: example, parse TypeScript  file
    (espresso-ve)
    
    with TypeScript 2.5 parser

--- Tests/Test3/Form1.Designer.cs
+++ Tests/Test3/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
             this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -146,13 +147,13 @@
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(121, 45);
             this.button10.TabIndex = 10;
-            this.button10.Text = "Test TypeScript";
+            this.button10.Text = "Test TypeScript2.1";
             this.button10.UseVisualStyleBackColor = true;
             this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
-            this.button11.Location = new System.Drawing.Point(581, 355);
+            this.button11.Location = new System.Drawing.Point(581, 406);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(121, 45);
             this.button11.TabIndex = 11;
@@ -160,11 +161,22 @@
             this.button11.UseVisualStyleBackColor = true;
             this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(581, 355);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(121, 45);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "Test TypeScript2.5";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(756, 506);
+            this.Controls.Add(this.button12);
             this.Controls.Add(this.button11);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
@@ -197,6 +209,7 @@
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
     }
 }
 
--- Tests/Test3/Form1.cs
+++ Tests/Test3/Form1.cs
@@ -562,7 +562,7 @@ namespace Test3
             //test tsc.js
             //this needs EspressoHostForTsc 
             //-----------------
-            string tsc_esprCode = File.ReadAllText("../../Samples/js_tools/tsc/tsc_espr.js");
+            string tsc_esprCode = File.ReadAllText("../../Samples/js_tools/tsc2_1/tsc_espr.js");
             StringBuilder stbuilder = new StringBuilder();
             stbuilder.Append(tsc_esprCode);
             //-----------------
@@ -580,7 +580,7 @@ namespace Test3
                 stwatch.Reset();
                 stwatch.Start();
 
-                var my_expr_ext = new EspressoHostForTsc();
+                var my_expr_ext = new EspressoHostForTsc_21();
                 ctx.SetVariableAutoWrap("my_expr_ext", my_expr_ext);
 
                 string testsrc = @"(function(){
@@ -606,7 +606,7 @@ namespace Test3
 
 
         [JsType]
-        class EspressoHostForTsc
+        class EspressoHostForTsc_21
         {
             //-------------------------------------
             //function getEspressoSystem()
@@ -928,5 +928,276 @@ namespace Test3
             }
         }
 
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //very basic ***
+            //-----------------
+            //test tsc.js
+            //this needs EspressoHostForTsc 
+            //-----------------
+            string tsc_esprCode = File.ReadAllText("../../Samples/js_tools/tsc2_5/tsc_espr.js");
+            StringBuilder stbuilder = new StringBuilder();
+            stbuilder.Append(tsc_esprCode);
+            //-----------------
+
+            int version = JsBridge.LibVersion;
+#if DEBUG
+            JsBridge.dbugTestCallbacks();
+#endif
+            using (JsEngine engine = new JsEngine())
+            using (JsContext ctx = engine.CreateContext(new MyJsTypeDefinitionBuilder()))
+            {
+
+                GC.Collect();
+                System.Diagnostics.Stopwatch stwatch = new System.Diagnostics.Stopwatch();
+                stwatch.Reset();
+                stwatch.Start();
+
+                var my_expr_ext = new EspressoHostForTsc_25();
+                ctx.SetVariableAutoWrap("my_expr_ext", my_expr_ext);
+
+                string testsrc = @"(function(){
+                       
+                        // test1: general  compile through commamd line
+                        // ts.executeCommandLine(['greeter.ts']);
+                        //-------------------------------------------------
+                        // test 2: generate ast 
+                        var filename=""greeter.ts"";  //example only
+                        my_expr_ext.ConsoleLog(filename);
+                        //parse
+                        const sourceFile = ts.createSourceFile(filename,
+                        my_expr_ext.readFile(filename),2, false);
+                        //send output as json to managed host
+                        my_expr_ext.ConsoleLog(JSON.stringify( sourceFile));
+                    })()";
+                stbuilder.Append(testsrc);
+                ctx.Execute(stbuilder.ToString());
+
+                stwatch.Stop();
+                Console.WriteLine("met1 template:" + stwatch.ElapsedMilliseconds.ToString());
+            }
+        }
+
+        [JsType]
+        class EspressoHostForTsc_25
+        {
+            //function getEspressoSystem()
+            //{
+            //    my_expr_ext.ConsoleLog("test");
+            //    var realpath = my_expr_ext.realpath && (function(path) { return my_expr_ext.realpath(path); });
+            //    return {
+            //        newLine: my_expr_ext.newLine || "\r\n",
+            //        args: my_expr_ext.GetArgs(),
+            //        useCaseSensitiveFileNames: !!my_expr_ext.useCaseSensitiveFileNames,
+            //        write: my_expr_ext.ConsoleLog,
+            //        readFile: function(path, _encoding) {
+            //            return my_expr_ext.readFile(path);
+            //        },
+            //        writeFile: function(path, data, writeByteOrderMark) {
+            //            if (writeByteOrderMark)
+            //            {
+            //                data = "\uFEFF" + data;
+            //            }
+            //            my_expr_ext.writeFile(path, data);
+            //        },
+            //    resolvePath: my_expr_ext.resolvePath,
+            //    fileExists: my_expr_ext.fileExists,
+            //    directoryExists: my_expr_ext.folderExists,
+            //    createDirectory: my_expr_ext.createFolder,
+            //    getExecutingFilePath: function() { return my_expr_ext.executingFile; },
+            //    getCurrentDirectory: function() { return my_expr_ext.GetCurrentDir(); },
+            //    getDirectories: my_expr_ext.getDirectories,
+            //     getEnvironmentVariable: function(vname){
+			//		return my_expr_ext.getEnvironmentVariable(vname);
+			//	},
+            //    readDirectory: function(path, extensions, excludes, includes, _depth) {
+            //            var pattern = ts.getFileMatcherPatterns(path, excludes, includes, !!my_expr_ext.useCaseSensitiveFileNames, my_expr_ext.GetCurrentDir());
+            //            return my_expr_ext.readDirectory(path, extensions, pattern.basePaths, pattern.excludePattern, pattern.includeFilePattern, pattern.includeDirectoryPattern);
+            //        },
+            //    exit: my_expr_ext.quit,
+            //    realpath: realpath
+            //    };
+            //}
+            [JsMethod]
+            public string[] readDirectory(string path, string extensions, string basePaths, string excludePattern, string includeFilePattern, string includeDirectoryPattern)
+            {
+                //TODO: impl here
+                return new string[0];
+            }
+            [JsMethod]
+            public string getEnvironmentVariable(string varname)
+            {
+                return "";//test
+            }
+            [JsMethod]
+            public string realPath(string relativePath)
+            {
+                return relativePath;
+            }
+            [JsMethod]
+            public string resolvePath(string relativePath)
+            {
+                return relativePath;
+            }
+            [JsProperty]
+            public string newLine
+            {
+                get
+                {
+                    return "\r\n";
+                }
+            }
+            [JsProperty]
+            public bool useCaseSensitiveFileNames
+            {
+                get
+                {
+                    return true;
+                }
+            }
+            [JsProperty]
+            public string executingFile
+            {
+                get
+                {
+                    return "";
+                }
+            }
+
+
+
+            [JsMethod]
+            public string readFile(string filename)
+            {
+                //string sampleFileContent1 = @"function greeter(person) {
+                //                           return ""Hello, "" + person;
+                //                     }
+                //                     var user = ""Jane User"";
+                //                     document.body.innerHTML = greeter(user);";
+
+                if (filename == "greeter.ts")
+                {
+                    string sampleFileContent2 =
+                        @"
+                        class Student {
+                            fullName: string;
+                            constructor(public firstName, public middleInitial, public lastName) {
+                                this.fullName = firstName + "" "" + middleInitial + "" "" + lastName;
+                            }
+                         }
+
+                        interface Person
+                        {
+                            firstName: string;
+                            lastName: string;
+                        }
+
+                        function greeter(person : Person)
+                        {
+                            return ""Hello, "" + person.firstName + "" "" + person.lastName;
+                        }
+
+                        var user = new Student(""Jane"", ""M."",""User"");
+                        document.body.innerHTML = greeter(user);
+                    ";
+                    return sampleFileContent2;
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            [JsMethod]
+            public void writeFile(string filename, string data)
+            {
+                Console.WriteLine("req: write " + filename);
+                Console.WriteLine("");
+                Console.WriteLine("=== compiler output===");
+                Console.WriteLine("");
+                Console.WriteLine(data);
+                Console.WriteLine("");
+                Console.WriteLine("======");
+                Console.WriteLine("");
+            }
+            [JsMethod]
+            public string getDirectories(string path)
+            {
+                //get directory
+                return "";
+            }
+            [JsMethod]
+            public string GetAccessibleFileSystemEntries(string path)
+            {
+                //return { files: files, directories: directories };
+                //        return JSON.parse(my_expr_ext.GetAccessibleFileSystemEntries(path));
+                //get directory
+
+                return "";
+            }
+            [JsMethod]
+            public string GetCurrentDir()
+            {
+                return ".";
+            }
+            [JsMethod]
+            public bool fileExists(string filename)
+            {
+                return false;
+            }
+            [JsMethod]
+            public bool directoryExists(string folderName)
+            {
+                return false;
+            }
+            [JsMethod]
+            public string GetScriptFullName()
+            {
+                return "hello1.ts";
+            }
+            [JsMethod]
+            public void createFolder(string directoryName)
+            {
+
+            }
+            [JsMethod]
+            public void quit(int exitCode)
+            {
+                Console.WriteLine("quit:" + exitCode);
+            }
+
+
+            [JsMethod]
+            public string GetArgs()
+            {
+                //return json string
+                return "[]";
+            }
+
+
+            [JsMethod]
+            public void ConsoleWrite(string msg)
+            {
+                Console.WriteLine("console write :" + msg);
+            }
+            [JsMethod]
+            public void ConsoleLog(object o)
+            {
+                Console.WriteLine("console write :" + o.ToString());
+            }
+            [JsMethod]
+            public object Require(string module)
+            {
+                //for require()
+                if (module == "fs")
+                {
+                    return this;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+        }
     }
 }
