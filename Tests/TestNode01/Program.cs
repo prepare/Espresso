ec01086fd7c0598cfcf122b1fda8faacd166ced7
win <wintercoredev@gmail.com>
2019-03-22 21:22:49 +0700

    refactor http2 example

--- Tests/TestNode01/Program.cs
+++ Tests/TestNode01/Program.cs
@@ -39,27 +39,10 @@ namespace TestNode01
             int libesprVer = JsBridge.LibVersion;
 #if DEBUG
             JsBridge.dbugTestCallbacks();
-#endif
-            //------------ 
-            NodeJsEngine.Run(
-                new string[] { "--inspect", "hello.espr" },
-                (eng, ctx) =>
-            {
-                //-------------
-                //this LibEspressoClass object is needed,
-                //so node can talk with us,
-                //------------- 
-                JsTypeDefinition jstypedef = new JsTypeDefinition("LibEspressoClass");
-                jstypedef.AddMember(new JsMethodDefinition("LoadMainSrcFile", args =>
-                {
-
-
-                    //handle main src loading here 
-                    //example 2:
-                    //test http2 --server side
-                    //(from https://nodejs.org/api/http2.html#http2_server_side_example)
-                    string filedata =
-                    @" const http2 = require('http2');
+#endif  
+            //------------
+            NodeJsEngineHelper.Run(new string[] { "--inspect", "hello.espr" },
+                ss => @" const http2 = require('http2');
                     const fs = require('fs');
 
                     const server = http2.createSecureServer({
@@ -79,35 +62,8 @@ namespace TestNode01
                     });
 
                     server.listen(8443);
-                    ";
-                    args.SetResult(filedata);
-                    //-------------------------
-
-                    //
-                    //example 1: simple http server
-                    //
-                    ////handle main src loading here
-                    //string filedata = @"   var http = require('http');
-                    //                            (function(){
-                    //                             console.log('hello from Espresso-ND');
-                    //                             var server = http.createServer(function(req, res) {
-                    //                                res.writeHead(200);
-                    //                                res.end('Hello! from Espresso-ND');
-                    //                                });
-                    //                                server.listen(8080,'localhost');
-                    //                            })();";
-                    //args.SetResult(filedata);
-                }));
-                ctx.RegisterTypeDefinition(jstypedef);
-                //----------
-                //then register this as x***       
-                //this object is just an instance for reference        
-                ctx.SetVariableFromAny("LibEspresso",
-                      ctx.CreateWrapper(new object(), jstypedef));
-            });
-
+                    ");
             string userInput = Console.ReadLine();
-
         }
 
 
