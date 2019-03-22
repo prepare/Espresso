da05602688ba68d8ce3a72a183b6a0f29bd37a51
win <wintercoredev@gmail.com>
2019-03-22 20:14:54 +0700

    add more node examples

--- Tests/TestNode02/Program.cs
+++ Tests/TestNode02/Program.cs
@@ -34,27 +34,17 @@ namespace TestNode01
             int errCode = GetLastError();
             int libesprVer = JsBridge.LibVersion;
 
-
-            //TestSocketIO_ChatExample(); 
+             
             //TestNodeVM_Example();
             //TestNodeFeature_OS_Example1();
-            TestNodeFeature_OS_Example2();
+            //TestNodeFeature_OS_Example2();
+            //TestNodeFeature_DNS_Example();
+            //TestNodeFeature_Internationalization_Example();
+            TestNodeFeature_Url_Example();
+
+            //TestSocketIO_ChatExample(); 
         }
-        static void TestSocketIO_ChatExample()
-        {
-            //change working dir to target app and run 
-            //test with socket.io's chat sample
-            System.IO.Directory.SetCurrentDirectory(@"../../../socket.io/examples/chat");
-
-#if DEBUG
-            JsBridge.dbugTestCallbacks();
-#endif
-            //------------ 
-
-            NodeJsEngineHelper.Run(ss => File.ReadAllText("index.js"));
-
-            string userInput = Console.ReadLine();
-        }
+       
 
 
         static void TestNodeVM_Example()
@@ -129,7 +119,7 @@ namespace TestNode01
                     console.log('cpus = '+ JSON.stringify(os.cpus()));
                     console.log('hostname='+ os.hostname());
                     ";
-            }); 
+            });
             string userInput = Console.ReadLine();
         }
         static void TestNodeFeature_OS_Example2()
@@ -139,7 +129,7 @@ namespace TestNode01
             JsBridge.dbugTestCallbacks();
 #endif
             //------------ 
- 
+
             ////example2: get value from node js 
             OsInfo myOsInfo = new OsInfo();
             NodeJsEngineHelper.Run(ss =>
@@ -151,11 +141,9 @@ namespace TestNode01
                     my_osInfo.Arch = os.arch();
                     my_osInfo.Hostname = os.hostname();
                     ";
-            }); 
+            });
             Console.WriteLine("arch=" + myOsInfo.Arch);
             Console.WriteLine("hostname=" + myOsInfo.Hostname);
-
-
             string userInput = Console.ReadLine();
         }
 
@@ -167,7 +155,90 @@ namespace TestNode01
             [JsProperty]
             public string Hostname { get; set; }
         }
+        //--------------------------
+        static void TestNodeFeature_DNS_Example()
+        {
+            //https://nodejs.org/dist/latest-v11.x/docs/api/dns.html
+#if DEBUG
+            JsBridge.dbugTestCallbacks();
+#endif
+            //------------ 
 
+            //example1: just show value
+            NodeJsEngineHelper.Run(() =>
+            {
+                return @"                     
+                    const dns = require('dns');
+
+                    dns.lookup('iana.org', (err, address, family) => {
+                        console.log('address: %j family: IPv%s', address, family);
+                    });
+                    
+                    ";
+            });
+            string userInput = Console.ReadLine();
+        }
+
+        //--------------------------
+        static void TestNodeFeature_Internationalization_Example()
+        {
+            //https://nodejs.org/dist/latest-v11.x/docs/api/intl.html
+#if DEBUG
+            JsBridge.dbugTestCallbacks();
+#endif
+            //------------ 
+
+            //example1: just show value
+            NodeJsEngineHelper.Run(() =>
+            {
+                return @"                     
+                    const january = new Date(9e8);
+                    const english = new Intl.DateTimeFormat('en', { month: 'long' });
+                    const spanish = new Intl.DateTimeFormat('es', { month: 'long' });
+
+                    console.log(english.format(january));
+                    // Prints 'January'
+                    console.log(spanish.format(january));
+                                    // Prints 'M01' on small-icu
+                                    // Should print enero
+
+                ";
+            });
+            string userInput = Console.ReadLine();
+        }
+
+        static void TestNodeFeature_Url_Example()
+        {
+            //https://nodejs.org/dist/latest-v11.x/docs/api/url.html
+#if DEBUG
+            JsBridge.dbugTestCallbacks();
+#endif
+            //------------ 
+
+            //example1: just show value
+            NodeJsEngineHelper.Run(() =>
+            {
+                return @"            
+
+                        const url = require('url');
+                        const myURL1 =
+                            url.parse('https://user:pass@sub.example.com:8080/p/a/t/h?query=string#hash');
+
+                        console.log(JSON.stringify(myURL1));
+
+                        //
+                        console.log('\r\n');
+                        console.log('\r\n');
+                        //
+                        const myURL2 =
+                            new URL('https://user:pass@sub.example.com:8080/p/a/t/h?query=string#hash');
+                        console.log(myURL2.host+'\r\n');
+                        console.log(myURL2.href+'\r\n');
+                        console.log(myURL2.hostname +'\r\n');
+                ";
+            });
+            string userInput = Console.ReadLine();
+        }
 
         private static void Proc_OutputDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
         {
@@ -179,6 +250,24 @@ namespace TestNode01
 
         }
 
+
+
+
+        static void TestSocketIO_ChatExample()
+        {
+            //change working dir to target app and run 
+            //test with socket.io's chat sample
+            System.IO.Directory.SetCurrentDirectory(@"../../../socket.io/examples/chat");
+
+#if DEBUG
+            JsBridge.dbugTestCallbacks();
+#endif
+            //------------ 
+
+            NodeJsEngineHelper.Run(ss => File.ReadAllText("index.js"));
+
+            string userInput = Console.ReadLine();
+        }
         [System.Runtime.InteropServices.DllImport("Kernel32.dll")]
         static extern IntPtr LoadLibrary(string dllname);
         [System.Runtime.InteropServices.DllImport("Kernel32.dll")]
