8b7eb0a7171e23b3b3ccf513446faa87eaddf59b
win <wintercoredev@gmail.com>
2021-01-15 07:50:34 +0700

    Update libespr_nodemain.cpp

--- libespresso/libespr_nodemain.cpp
+++ libespresso/libespr_nodemain.cpp
@@ -25,83 +25,93 @@
 #include <VersionHelpers.h>
 #include <WinError.h>
 
-int mywinmain(int argc, wchar_t *wargv[]) {
-	if (!IsWindows7OrGreater()) {
-		fprintf(stderr, "This application is only supported on Windows 7, "
-			"Windows Server 2008 R2, or higher.");
-		exit(ERROR_EXE_MACHINE_TYPE_MISMATCH);
-	}
+#define SKIP_CHECK_VAR "NODE_SKIP_PLATFORM_CHECK"
+#define SKIP_CHECK_SIZE 1
+#define SKIP_CHECK_VALUE "1"
 
-	// Convert argv to to UTF8
-	char** argv = new char*[argc + 1];
-	for (int i = 0; i < argc; i++) {
-		// Compute the size of the required buffer
-		DWORD size = WideCharToMultiByte(CP_UTF8,
-			0,
-			wargv[i],
-			-1,
-			nullptr,
-			0,
-			nullptr,
-			nullptr);
-		if (size == 0) {
-			// This should never happen.
-			fprintf(stderr, "Could not convert arguments to utf8.");
-			exit(1);
-		}
-		// Do the actual conversion
-		argv[i] = new char[size];
-		DWORD result = WideCharToMultiByte(CP_UTF8,
-			0,
-			wargv[i],
-			-1,
-			argv[i],
-			size,
-			nullptr,
-			nullptr);
-		if (result == 0) {
-			// This should never happen.
-			fprintf(stderr, "Could not convert arguments to utf8.");
-			exit(1);
-		}
-	}
-	argv[argc] = nullptr;
-	// Now that conversion is done, we can finally start.
-	return node::Start(argc, argv);
+int mywinmain(int argc, wchar_t *wargv[]) {
+  // Windows Server 2012 (not R2) is supported until 10/10/2023, so we allow it
+  // to run in the experimental support tier.
+  char buf[SKIP_CHECK_SIZE + 1];
+  if (!IsWindows8Point1OrGreater() &&
+      !(IsWindowsServer() && IsWindows8OrGreater()) &&
+      (GetEnvironmentVariableA(SKIP_CHECK_VAR, buf, sizeof(buf)) !=
+           SKIP_CHECK_SIZE ||
+       strncmp(buf, SKIP_CHECK_VALUE, SKIP_CHECK_SIZE + 1) != 0)) {
+    fprintf(stderr,
+            "Node.js is only supported on Windows 8.1, Windows "
+            "Server 2012 R2, or higher.\n"
+            "Setting the " SKIP_CHECK_VAR " environment variable "
+            "to 1 skips this\ncheck, but Node.js might not execute "
+            "correctly. Any issues encountered on\nunsupported "
+            "platforms will not be fixed.");
+    exit(ERROR_EXE_MACHINE_TYPE_MISMATCH);
+  }
+
+  // Convert argv to UTF8
+  char** argv = new char*[argc + 1];
+  for (int i = 0; i < argc; i++) {
+    // Compute the size of the required buffer
+    DWORD size = WideCharToMultiByte(
+        CP_UTF8, 0, wargv[i], -1, nullptr, 0, nullptr, nullptr);
+    if (size == 0) {
+      // This should never happen.
+      fprintf(stderr, "Could not convert arguments to utf8.");
+      exit(1);
+    }
+    // Do the actual conversion
+    argv[i] = new char[size];
+    DWORD result = WideCharToMultiByte(
+        CP_UTF8, 0, wargv[i], -1, argv[i], size, nullptr, nullptr);
+    if (result == 0) {
+      // This should never happen.
+      fprintf(stderr, "Could not convert arguments to utf8.");
+      exit(1);
+    }
+  }
+  argv[argc] = nullptr;
+  // Now that conversion is done, we can finally start.
+  return node::Start(argc, argv);
 }
 #else
 // UNIX
 #ifdef __linux__
-#include <elf.h>
-#ifdef __LP64__
-#define Elf_auxv_t Elf64_auxv_t
-#else
-#define Elf_auxv_t Elf32_auxv_t
-#endif  // __LP64__
-extern char** environ;
+#include <sys/auxv.h>
 #endif  // __linux__
+#if defined(__POSIX__) && defined(NODE_SHARED_MODE)
+#include <signal.h>
+#include <string.h>
+#endif
 
 namespace node {
-	extern bool linux_at_secure;
+namespace per_process {
+extern bool linux_at_secure;
+}  // namespace per_process
 }  // namespace node
 
-int myunixmain(int argc, char *argv[]) {
-#if defined(__linux__)
-	char** envp = environ;
-	while (*envp++ != nullptr) {}
-	Elf_auxv_t* auxv = reinterpret_cast<Elf_auxv_t*>(envp);
-	for (; auxv->a_type != AT_NULL; auxv++) {
-		if (auxv->a_type == AT_SECURE) {
-			node::linux_at_secure = auxv->a_un.a_val;
-			break;
-		}
-	}
+int main(int argc, char* argv[]) {
+#if defined(__POSIX__) && defined(NODE_SHARED_MODE)
+  // In node::PlatformInit(), we squash all signal handlers for non-shared lib
+  // build. In order to run test cases against shared lib build, we also need
+  // to do the same thing for shared lib build here, but only for SIGPIPE for
+  // now. If node::PlatformInit() is moved to here, then this section could be
+  // removed.
+  {
+    struct sigaction act;
+    memset(&act, 0, sizeof(act));
+    act.sa_handler = SIG_IGN;
+    sigaction(SIGPIPE, &act, nullptr);
+  }
 #endif
-	// Disable stdio buffering, it interacts poorly with printf()
-	// calls elsewhere in the program (e.g., any logging from V8.)
-	setvbuf(stdout, nullptr, _IONBF, 0);
-	setvbuf(stderr, nullptr, _IONBF, 0);
-	return node::Start(argc, argv);
+
+#if defined(__linux__)
+  node::per_process::linux_at_secure = getauxval(AT_SECURE);
+#endif
+  // Disable stdio buffering, it interacts poorly with printf()
+  // calls elsewhere in the program (e.g., any logging from V8.)
+  setvbuf(stdout, nullptr, _IONBF, 0);
+  setvbuf(stderr, nullptr, _IONBF, 0);
+  return node::Start(argc, argv);
 }
 #endif
 //============================================================
